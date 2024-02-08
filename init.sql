c9a949d3c2867b43300867ffec3aeb25435b819f
jonathan.marin <jonathan.marin.c@gmail.com>
2024-02-08 00:56:53 -0500

    fix db column names to snake case

--- init.sql
+++ init.sql
@@ -1,32 +1,32 @@
 CREATE TABLE author
 (
-    authorId bigint NOT NULL,
-    authorName text COLLATE pg_catalog."default",
-    CONSTRAINT author_pkey PRIMARY KEY (authorId)
+    author_id bigint NOT NULL,
+    author_name text COLLATE pg_catalog."default",
+    CONSTRAINT author_pkey PRIMARY KEY (author_id)
 );
 
-INSERT INTO author(authorId, authorName) VALUES
+INSERT INTO author(author_id, author_name) VALUES
  (1, 'Jonathan Marin Cermeno'),
  (2, 'Jhon Connor'),
  (3, 'Agent Smith');
  
  CREATE TABLE public.movie (
-	movieid bigint NOT NULL,
-	moviename varchar NULL,
-	movieyear information_schema."time_stamp" NULL,
-	authorid bigint NOT NULL,
-	CONSTRAINT movie_pk PRIMARY KEY (movieid),
-	CONSTRAINT movie_unique UNIQUE (authorid),
-	CONSTRAINT movie_author_fk FOREIGN KEY (authorid) REFERENCES public.author(authorid)
+	movie_id bigint NOT NULL,
+	movie_name varchar NULL,
+	movie_year information_schema."time_stamp" NULL,
+	author_id bigint NOT NULL,
+	CONSTRAINT movie_pk PRIMARY KEY (movie_id),
+	CONSTRAINT movie_unique UNIQUE (author_id),
+	CONSTRAINT movie_author_fk FOREIGN KEY (author_id) REFERENCES public.author(author_id)
 );
 
 INSERT INTO public.movie
-(movieid, moviename, movieyear, authorid)
+(movie_id, movie_name, movie_year, author_id)
 VALUES(1, 'Back to the Future', '2024-02-07 03:41:22.030', 1);
 INSERT INTO public.movie
-(movieid, moviename, movieyear, authorid)
+(movie_id, movie_name, movie_year, author_id)
 VALUES(2, 'Terminator', '2024-02-07 03:42:57.420', 2);
 INSERT INTO public.movie
-(movieid, moviename, movieyear, authorid)
+(movie_id, movie_name, movie_year, author_id)
 VALUES(3, 'Matrix', '2024-02-07 03:42:57.420', 3);
 
